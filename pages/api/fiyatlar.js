b576c03136419abb1f089b1daf66334d172498af
onur846 <115958771+onur846@users.noreply.github.com>
2025-04-27 06:45:27 +0300

    Update fiyatlar.js

--- pages/api/fiyatlar.js
+++ pages/api/fiyatlar.js
@@ -3,15 +3,20 @@ import cheerio from 'cheerio';
 import fs from 'fs';
 import path from 'path';
 
+const axiosInstance = axios.create({
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36'
+  }
+});
+
 export default async function handler(req, res) {
   try {
     const baseUrl = 'https://fpprotr.com/cihazimi-tamir-et/iphone-serileri/';
-    const { data } = await axios.get(baseUrl);
+    const { data } = await axiosInstance.get(baseUrl);
     const $ = cheerio.load(data);
 
     const fiyatlar = {};
 
-    // Ana sayfadaki iPhone serilerinin linklerini bulalım
     const seriLinkleri = [];
     $('a.elementor-button-link').each((i, el) => {
       const link = $(el).attr('href');
@@ -20,9 +25,8 @@ export default async function handler(req, res) {
       }
     });
 
-    // Her seri linkine gidip modelleri ve fiyatları çekelim
     for (const link of seriLinkleri) {
-      const { data: pageData } = await axios.get(link);
+      const { data: pageData } = await axiosInstance.get(link);
       const $$ = cheerio.load(pageData);
 
       $$('.elementor-widget-container').each((i, el) => {
@@ -43,18 +47,15 @@ export default async function handler(req, res) {
       });
     }
 
-    // Şimdi database.json içeriğini de okuyalım
     const filePath = path.join(process.cwd(), 'public', 'database.json');
     const jsonData = fs.readFileSync(filePath, 'utf8');
     const parsedData = JSON.parse(jsonData);
 
-    // TR Cihazı Fiyatları ekle
     for (const model in parsedData.trCihazFiyatlari) {
       if (!fiyatlar[model]) fiyatlar[model] = {};
       fiyatlar[model]["TR Cihazı Full Fiyatı"] = parsedData.trCihazFiyatlari[model];
     }
 
-    // Anakart Fiyatları ekle
     for (const model in parsedData.anakartFiyatlari) {
       if (!fiyatlar[model]) fiyatlar[model] = {};
       fiyatlar[model]["Anakart Onarım"] = parsedData.anakartFiyatlari[model];
